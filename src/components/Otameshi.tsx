960effe8e651bbf1ef146771bc2a1411e0cf2601
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-17 22:59:13 +0900

    add : add Otameshi axios

--- src/components/Otameshi.tsx
+++ src/components/Otameshi.tsx
@@ -1,44 +1,111 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
+const searchGoogleBooks = async (searchString: string) => {
+  const url = 'https://www.googleapis.com/books/v1/volumes';
+  const params = { q: searchString };
+  try {
+    const response = await axios.get(url, { params });
+    return { isSuccess: true, data: response.data, error: null };
+  } catch (error) {
+    return { isSuccess: false, data: null, error };
+  }
+};
 export const Otameshi: React.FC = () => {
-  const [text, changeText] = useState('');
+  const [searchString, changeSearchString] = useState('');
+  const [searchResult, changeSearchResult] = useState<any>(null);
+
+  const handleOnSearchButton = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // form要素のbuttonのsubmitを止める
+    event.preventDefault();
+    const result = await searchGoogleBooks(searchString);
+    if (result.isSuccess) {
+      changeSearchResult(result.data);
+    } else {
+      window.alert(String(result.error));
+    }
+  };
+
   return (
     <Wrapper>
       <Body>
-        <Title>Otameshi Component</Title>
+        <Title>Google Books 検索</Title>
 
-        <TextArea placeholder='テキストを入力してね！' onChange={(event): void => changeText(event.target.value)} />
+        <SearchForm>
+          <Input placeholder='検索ワードを入力してね！' onChange={event => changeSearchString(event.target.value)} />
+          <SearchButton onClick={event => handleOnSearchButton(event)} disabled={!searchString}>
+            検索
+          </SearchButton>
+        </SearchForm>
 
-        <TextResult>{text}</TextResult>
+        {searchResult && (
+          <ResultContent>
+            {searchResult.items.map((item: any) => {
+              return <ResultTitle key={item.id}>{item.volumeInfo.title}</ResultTitle>;
+            })}
+          </ResultContent>
+        )}
       </Body>
     </Wrapper>
   );
 };
 
+const ResultContent = styled.div`
+  margin-top: 20px;
+`;
+
+const ResultTitle = styled.div`
+  padding: 10px 0;
+  border-bottom: 1px solid;
+  &:first-of-type {
+    border-top: 1px solid;
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
+  margin-top: 20px;
 `;
 
 const Body = styled.div``;
 
 const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
   text-align: center;
 `;
 
-const TextArea = styled.textarea`
+const Input = styled.input`
   display: block;
-  margin: 0 auto;
   box-sizing: border-box;
-  width: 200px;
+  width: 250px;
+  font-size: 18px;
+  padding: 10px;
+  outline: none;
 `;
 
-const TextResult = styled.p`
-  width: 200px;
+const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+const SearchButton = styled.button`
+  color: #fff;
+  background-color: #09d3ac;
+  border-radius: 3px;
+  margin-left: 10px;
   padding: 10px;
-  margin: 20px auto;
-  border: 1px solid blue;
-  white-space: pre-wrap;
-  box-sizing: border-box;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  transition: 0.4s;
+  cursor: pointer;
+  &:disabled {
+    background-color: #bfbfbf;
+    cursor: not-allowed;
+  }
 `;
